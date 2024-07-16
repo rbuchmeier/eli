506924e00f6fbb63395bcb6f918d647a411bd592
Ryan Buchmeier <ryan.b@verytechnology.com>
2024-07-16 17:46:08 -0600

    Added rgb support with minor cleanup

--- index.spec.ts
+++ index.spec.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from "vitest";
-import {colorInverter, hexidexSubtractor} from "./index";
+import {colorInverter, hexidexSubtractor, hexParser} from "./index";
 
 describe("index", () => {
     it("should work", () => {
@@ -9,16 +9,24 @@ describe("index", () => {
         it("should turn black to white", () => {
             expect(colorInverter("#000000")).toBe("#ffffff");
         });
-        // it("should turn white to black", () => {
-        //     expect(colorInverter("#fffff0")).toBe("#00000f");
-        // });
+        it("should turn white to black", () => {
+            expect(colorInverter("#fffff0")).toBe("#00000f");
+        });
     });
     describe("hexidexSubtractor", () => {
         it("should subtract 2 hexidecimal numbers", () => {
-            expect(hexidexSubtractor("ff", "ff")).toBe("0");
+            expect(hexidexSubtractor("ff", "ff")).toBe("00");
         });
         it("should subtract 3 from ff", () => {
             expect(hexidexSubtractor("ff", "03")).toBe("fc");
+        });
+        it("should return 2 chars for 15", () => {
+            expect(hexidexSubtractor("ff", "f0")).toBe("0f");
+        });
+    });
+    describe('hexParser', () => {
+        it('should parse a hex string', () => {
+            expect(hexParser('#ff00ff')).toEqual(['ff', '00', 'ff']);
         });
     });
 });
--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
-export const colorInverter = (color: string) => {
-    return color === "#000000" ? "#ffffff" : "#000000";
+type colorInverterParams = string | Number[];
+export const colorInverter = <T extends colorInverterParams>(color: T): T => {
+    if (typeof color === "string") {
+        const [color1, color2, color3] = hexParser(color);
+        const invertedColor1 = hexidexSubtractor("ff", color1);
+        const invertedColor2 = hexidexSubtractor("ff", color2);
+        const invertedColor3 = hexidexSubtractor("ff", color3);
+        return `#${invertedColor1}${invertedColor2}${invertedColor3}` as T;
+    } else {
+        return [255 - color[0], 255 - color[1], 255 - color[2]] as T;
+    }
 }
 
-export const hexidexSubtractor = (hex1: string, hex2: string) => {
+export const hexidexSubtractor = (hex1: string, hex2: string):string => {
     const num1 = parseInt(hex1, 16);
     const num2 = parseInt(hex2, 16);
-    return (num1 - num2).toString(16);
+    return (num1 - num2).toString(16).padStart(2, '0');
+}
+
+export const hexParser = (hex: string) => {
+    return [hex.substring(1, 3), hex.substring(3, 5), hex.substring(5, 7)];
 }
